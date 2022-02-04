c366210420c68b89d02cebacea05aeaaa9a09da1
Ayaan619 <ayaanaffandi@gmail.com>
2022-02-04 16:10:53 +0530

    First Commit

---
+++ jewel-app/src/app/login-form/login-form.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-login-form',
+  templateUrl: './login-form.component.html',
+  styleUrls: ['./login-form.component.css']
+})
+export class LoginFormComponent implements OnInit {
+
+  constructor(){}
+
+  //loginUser(value:any ){
+   // console.warn(value);
+    
+  //} 
+  loginUser!:FormGroup
+  ngOnInit(): void {
+      
+  }
+  loginData(){
+
+  }
+}
